247febe3252266a388a2834100db906337cf6448
nealcosam <neal.coleman@partner.sea.samsung.com>
2017-06-29 15:07:58 -0500

    Update example.js

--- sdk-webrtc-onvideo-master/sdk-webrtc-onvideo-master/example.js
+++ sdk-webrtc-onvideo-master/sdk-webrtc-onvideo-master/example.js
@@ -15,7 +15,7 @@ define([
 
 ], function($, _, RTCManager, defaultRTCParams, BJN, RTCClient ) {
 
-    console.log("(example.js): BJN WebRTC Example 101");
+    console.log("(androidExample.js): BJN WebRTC Example");
 	
 	$("#joinMeeting, #leaveMeeting").click(function(){
 		$(this).addClass("hidden");
@@ -75,6 +75,9 @@ define([
 			evtLocalConnectionStateChange : null,
 			evtOnError : null
 		});
+		
+		// Save for external access
+		BJN.RTCClient = RTCClient;
 	}, function(error) {
 		console.log("Local device error " + error);
 		reject(error);
@@ -83,17 +86,50 @@ define([
 	
 	//------------------------------------------------------------------
 	// JQuery event handler intercepts
+	//   The intercepts handle the DOM events from GUI controls, then
+	// invoke the corresponding explosed javascript handler.
 	//
 	
 	// Device and Connection UI Handlers
-	$("#audioIn").change( changeAudioIn );
-	$("#audioOut").change(changeAudioOut );
-	$("#videoIn").change( changeVideoIn );
-	$("#videoBw").change( setVideoBandwidth );
+	$("#audioIn").change( function() {
+		var who = $("#audioIn").prop('selectedIndex');
+		changeAudioIn(who);
+	});
+	$("#audioOut").change( function() {
+		var who = $("#audioOut").prop('selectedIndex');
+		changeAudioOut(who);
+	});
+	$("#videoIn").change( function() {
+		var who = $("#videoIn").prop('selectedIndex');
+		changeVideoIn(who);
+	});
+	$("#videoBw").change( function() {
+		var bw = $("#videoBw").prop('value');
+		setVideoBandwidth(bw);
+	});
 	
 	// Mute UI handlers
-	$("#toggleAudioMute").click( toggleAudioMute );
-    $("#toggleVideoMute").click( toggleVideoMute );
+	$("#toggleAudioMute").click( function() {
+		var updatedText = toggleAudioMute() ? "Unmute Audio" : "Mute Audio";
+		$("#toggleAudioMute").html(updatedText);		
+	});
+    $("#toggleVideoMute").click( function() {
+		var muted = toggleVideoMute();
+		if(muted){
+			setMuteButton(muted);
+		}
+	});
+	
+	function setMuteButton(muted){
+		var updatedText = muted ? "Show Video" : "Mute Video";
+		$("#toggleVideoMute").html(updatedText);	
+	};
+
+	function unmuteVideo() {
+		setMuteButton(false);
+	};
+
+
 	// Meeting UI handlers
     $("#joinMeeting").click( function() {
 		var meetingParams = {
@@ -101,92 +137,107 @@ define([
             attendeePasscode    : $('#passCode').val(),
             displayName : $('#yourName').val()
         };
-		joinMeeting(mp);
+		joinMeeting(meetingParams);
     });
     $("#leaveMeeting").click( leaveMeeting );
 	
 	//
 	//------------------------------------------------------------------
-
-
-	//------------------------------------------------------------------
-	// discrete Javascript handlers mapping UI actions to BlueJeans commands
-	//	
-	
-	changeAudioIn = function(){
-		var who = $("#audioIn").prop('selectedIndex');
-		console.log("UI: audio input changed: " + who);
-		RTCClient.changeAudioInput(who);
-	};
-
-	changeAudioOut = function() {
-		var who = $("#audioOut").prop('selectedIndex');
-		console.log("UI: audio output changed: " + who );
-		RTCClient.changeAudioOutput(who);
-	};
-	
-	changeVideoIn = function() {
-		var who = $("#videoIn").prop('selectedIndex');
-		console.log("UI: video input changed: " + who );
-		RTCClient.changeVideoInput(who);
-	};
-	
-	setVideoBandwidth = function() {
-		var bw = $("#videoBw").prop('value');
-		console.log("UI: Video BW is changed " + bw);
-		RTCClient.setVideoBandwidth(bw);
-	};
-	
-	
-	toggleAudioMute = function() {
-        var muted = RTCClient.toggleAudioMute();
-		var updatedText = muted ? "Unmute Audio" : "Mute Audio";
-		$("#toggleAudioMute").html(updatedText);
-		console.log(muted ? "Audio is Muted now" : "Audio is Unmuted now");	
-    };
-
-	
-	toggleVideoMute = function() {
-        var muted = RTCClient.toggleVideoMute();
-		if(muted)
-			setMuteButton(muted);
-	};
-	
-	function unmuteVideo() {
-		setMuteButton(false);
-	};
-	
-	function setMuteButton(muted){
-		var updatedText = muted ? "Show Video" : "Mute Video";
-		$("#toggleVideoMute").html(updatedText);	
-    };
-	
-	////////////////// neal test //////////////////
-	function nealTestFunction() {
-		console.log("nealTestFunction() called");
-	};
-	
-	
-	window.nealTestFunction2 = function() {
-		console.log("nealTestFunction 2222() called");
-	};
-	////////////////// neal test //////////////////
-	
-	
-/*
-   the mp parameter is a JSON object with these fields:
-	{
-			numericMeetingId   : <string> the BlueJeans meeting Identifier,
-            attendeePasscode   : <string> <opt> the passcode for meeeting -or- moderator's code,
-            displayName        : <string> the name you want shown on roster 
-	}
-*/	
-	joinMeeting = function( mp ) {
-        RTCClient.joinMeeting(mp);
-	};
-	
-	leaveMeeting = function() {
-        RTCClient.leaveMeeting();
-	};
-
 });
+
+
+//------------------------------------------------------------------
+// discrete Javascript handlers providing exposure to
+//		- map UI actions to BlueJeans commands
+//		- access GUI/RTC related variables
+//	
+
+/* listLocalDevices() 
+	returns the JSON object containing the system information for the requested devices.
+		name is one of "audioIn", "audioOut", or "videoIn"
+*/
+listLocalDevices = function(name){
+	var retVal = {};
+	console.log("Listing local devices: " + name);
+	switch(name){
+		case "audioIn" :
+		   retVal = jQuery.extend(true, {}, BJN.localDevices.audioIn);
+		break;
+		case "audioOut" :
+		   retVal = jQuery.extend(true, {}, BJN.localDevices.audioOut);
+		break;
+		case "videoIn" :
+		   retVal = jQuery.extend(true, {}, BJN.localDevices.videoIn);
+		break;
+		default:
+		   retVal = {};
+		break;
+	};
+	return retVal;
+};
+
+/* changeAudioIn(index), changeAudioOut(index), changeVideoIn(index)
+	these changeXXXXIn/Out() functions take an integer index value
+	that corresponds to the device systems' localDevices collection.
+
+	The exhaustive list of XXXXXIn/Out devices can be retrieved from
+	the listLocalDevices() function call.   
+*/
+changeAudioIn = function(index){
+	console.log("audio input change: " + index);
+	BJN.RTCClient.changeAudioInput(index);
+};
+
+changeAudioOut = function(index) {
+	console.log("audio output change: " + index );
+	BJN.RTCClient.changeAudioOutput(index);
+};
+
+changeVideoIn = function(index) {
+	console.log("video input change: " + index );
+	BJN.RTCClient.changeVideoInput(index);
+};
+
+/* setVideoBandwidth()
+ the bw parameter is an integer specifying the network bitrate for the
+ video stream.  It should be in the range of 100..4096Kbps
+*/	
+setVideoBandwidth = function(bw) {
+	console.log("Video BW is changed to: " + bw + "Kbits/sec");
+	BJN.RTCClient.setVideoBandwidth(bw);
+};
+
+
+toggleAudioMute = function() {
+	var muted = BJN.RTCClient.toggleAudioMute();
+	console.log(muted ? "Audio is Muted now" : "Audio is Unmuted now");	
+	return muted;
+};
+
+
+toggleVideoMute = function() {
+	var muted = BJN.RTCClient.toggleVideoMute();
+	console.log( muted ? "Video mute called" : "Video unmute called");
+	return muted;
+};
+
+
+
+/* joinMeeting(mp)
+	Join a BJN meeting as specified by the the JSON mp parameter with fields:
+	  mp = {
+			numericMeetingId   : <string> the BlueJeans meeting Identifier,
+			attendeePasscode   : <string> <opt> the passcode for meeeting -or- moderator's code,
+			displayName        : <string> the name you want shown on roster 
+		   }
+*/	
+joinMeeting = function( mp ) {
+	console.log( "Requesting to join meeting: " + mp.numericMeetingId );
+	BJN.RTCClient.joinMeeting(mp);
+};
+
+leaveMeeting = function() {
+	console.log( "Leaving current meeting");
+	BJN.RTCClient.leaveMeeting();
+};
+
